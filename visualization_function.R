8c56dc003de4dffe4b6c0cc676bb544d8cfe5df5
Rose Determan <rosed@bu.edu>
2021-10-02 08:25:34 -0400

    Finished presentation.Rmd

--- visualization_function.R
+++ visualization_function.R
@@ -10,42 +10,42 @@ library(esquisse)
 
 library(plotly)
 library(tidyverse)
-library(htmlwidgets)
+#library(htmlwidgets)
 
-#esquisser(data = lit_dec, viewer = "browser")
+#esquisser(data = jn, viewer = "browser")
 
 
 rates_scatter <- function(data){
-    plt <- ggplot(data) +
-      aes(x = lit_rate, y = pov_rate,
-          text = paste(
-        "Country: ", country, "\n",
-        "Year: ", year, "\n",
-        "Literacy Rate: ", lit_rate, "\n",
-        "Poverty Rate: ", pov_rate, "\n",
-        sep = ""
-      ),
-      fill = decade) +
-      geom_point(shape = "circle", size = 1.5, colour = "#112446", stroke = .1, alpha = 0.75) +
-      labs(
-        x = "Literacy Rate",
-        y = "Poverty Rate",
-        title = "Literacy Rate vs Poverty Rate"
-      ) + 
-      scale_fill_brewer(palette = "Greys", direction = 1)+
-      theme_minimal()
+  knitr::opts_chunk$set(fig.width = 20,fig.height = 4)
+  plt <-  ggplot(jn) +
+    aes(x = lit_rate, y = pov_rate, colour = Region,
+        text = paste(
+          "Country: ", country, "\n",
+          "Year: ", year, "\n",
+          "Literacy Rate: ", lit_rate, "\n",
+          "Poverty Rate: ", pov_rate, "\n",
+          sep = ""
+        )) +
+    geom_point(shape = "circle", size = 2, alpha = 0.85) +
+    scale_color_brewer(palette = "Dark2", direction = 1) +
+    labs(
+      x = "Literacy Rate",
+      y = "Poverty Rate",
+      title = "Figure 2: Literacy Rate vs Poverty Rate")+
+    theme_minimal()
+  
     ggplotly(plt,tooltip = "text")
 }
 
 #rates_violin(lit_dec)
 rates_violin <- function(data){
   ggplot(data) +
-    aes(x = decade, y = mean_lit) +
+    aes(x = decade, y = lit_rate) +
     geom_violin(adjust = 1L, scale = "area", fill = "#112446") +
     labs(
       x = "Decade",
       y = "Literacy Rate",
-      title = "Density of Literacy Rates by Decade"
+      title = "Figure 1: Density of Literacy Rates by Decade"
     ) +
     theme_minimal()
 }
